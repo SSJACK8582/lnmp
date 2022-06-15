78e6e578bad2ad352ac6b12f9b2146299decd668
SSJACK8582 <1805330812@qq.com>
2022-06-15 17:14:06 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "ansible_host" "salt-proxy" {
   count = 1
 
   // 配置机器的 hostname，一般配置为计算资源的 public_ip (或 private_ip)
-  inventory_hostname  = alicloud_instance.ecsone[count.index].public_ip
+  inventory_hostname  = "60.205.176.114"
 
   // 配置机器所属分组
   groups = ["salt-proxy"]
@@ -23,7 +23,7 @@ resource "ansible_host" "salt-proxy" {
   // 传给 ansible 的 vars，可在 playbook 文件中引用
   vars = {
     wait_connection_timeout   = 60
-    proxy_private_ip          = alicloud_instance.ecsone[count.index].private_ip
+    proxy_private_ip          = "60.205.176.114"
     proxy_docker_tag          = var.proxy_docker_tag
   }
 }
